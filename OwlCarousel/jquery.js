67f8800dfda712b014c9a12dcd0868077fb8b611
adelpangestu <pangestuadellya7@gmail.com>
2021-01-05 15:58:45 +0700

    Landing_Page_factory

---
+++ OwlCarousel/jquery.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 5,
+    nav: false,
+    autoplay: true,
+    dots: false,
+    responsive: {
+        0: {
+            items: 2
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 4
+        }
+    }
+})
+
+
---
+++ script.js
@@ -0,0 +1,33 @@
+//back to top
+const backToTopButton = document.querySelector("#back-to-top-btn");
+
+window.addEventListener("scroll", scrollFunction);
+
+function scrollFunction() {
+    if (window.pageYOffset > 300) { //show backtotopbutton
+        if (!backToTopButton.classList.contains("btnEntrance")) {
+            backToTopButton.classList.remove("btnExit");
+            backToTopButton.classList.add("btnEntrance");
+            backToTopButton.style.display = "block";
+        }
+    }
+    else { //hide backtotopbutton
+        if (backToTopButton.classList.contains("btnEntrance")) {
+            backToTopButton.classList.remove("btnEntrance");
+            backToTopButton.classList.add("btnExit");
+            setTimeout(function () {
+                backToTopButton.style.display = "none";
+            }, 250);
+
+        }
+    }
+}
+
+backToTopButton.addEventListener("click", backToTop);
+
+function backToTop() {
+    window.scrollTo(0, 0);
+};
+
+
+
